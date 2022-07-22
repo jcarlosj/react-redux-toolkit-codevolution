a631020ee548afc0b204d131d5ad2610d5fb9d59
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-22 10:14:51 -0500

    [+] Components (CakeView, IcecreamView): Implementa acciones para ordenar/reponer pasteles y/o helados usando el hook useDispatch de React-Redux

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,15 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { ordered, restocked } from './cakeSlice';
 
 
 const CakeView = () => {
     // ? useSelector: Permite extraer datos del estado de la tienda Redux
     const numOfCakes = useSelector( ( state ) => state.cake.numOfCakes );   // ? state.<key-reducer>.<property-value>
 
+    // ? useDispatch: Devuelve una referencia a la función de despacho de la tienda Redux. Puede usarlo para enviar acciones según sea necesario.
+    const dispatch = useDispatch();
+
     return (
         <div>
             <h3>Number of cakes - { numOfCakes }</h3>
-            <button>Order cake</button>
-            <button>Restock cakes</button>
+            <button
+                onClick={ () => dispatch( ordered() ) }
+            >Order cake with ice cream</button>
+            <button
+                onClick={ () => dispatch( restocked( 5 ) )}
+            >Restock 5 cakes</button>
         </div>
     );
 };
--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,15 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { ordered, restocked } from './icecreamSlice';
 
 
 const IcecreamView = () => {
     // ? useSelector: Permite extraer datos del estado de la tienda Redux
     const numOfIcecreams = useSelector( state => state.icecream.numOfIceCreams );    // ? state.<key-reducer>.<property-value>
 
+    // ? useDispatch: Devuelve una referencia a la función de despacho de la tienda Redux. Puede usarlo para enviar acciones según sea necesario.
+    const dispatch = useDispatch();
+
     return (
         <div>
             <h3>Number of ice creams - { numOfIcecreams }</h3>
-            <button>Order ice cream</button>
-            <button>Restock ice creams</button>
+            <button
+                onClick={ () => dispatch( ordered() ) }
+            >Order ice cream</button>
+            <button
+                onClick={ () => dispatch( restocked( 3 ) )}
+            >Restock 3 ice creams</button>
         </div>
     );
 };
