3a6d0c314ab94cf65237af90d19e8d5aa34095c7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-22 09:59:47 -0500

    [+] Components (CakeView, IcecreamView): Extrae y muestra datos del Redux Store usando el hooks useSelector de React-Redux

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux';
+
+
 const CakeView = () => {
+    // ? useSelector: Permite extraer datos del estado de la tienda Redux
+    const numOfCakes = useSelector( ( state ) => state.cake.numOfCakes );   // ? state.<key-reducer>.<property-value>
+
     return (
         <div>
-            <h3>Number of cakes - </h3>
+            <h3>Number of cakes - { numOfCakes }</h3>
             <button>Order cake</button>
             <button>Restock cakes</button>
         </div>
--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux';
+
+
 const IcecreamView = () => {
+    // ? useSelector: Permite extraer datos del estado de la tienda Redux
+    const numOfIcecreams = useSelector( state => state.icecream.numOfIceCreams );    // ? state.<key-reducer>.<property-value>
+
     return (
         <div>
-            <h3>Number of ice creams - </h3>
+            <h3>Number of ice creams - { numOfIcecreams }</h3>
             <button>Order ice cream</button>
             <button>Restock ice creams</button>
         </div>
